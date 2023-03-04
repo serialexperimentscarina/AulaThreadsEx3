9f70be2961ece962edc1ab2ee1020ba0a0fcd887
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-03-04 17:32:34 -0300

    feat: Create "Principal"

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,8 +1,17 @@
 package view;
 
+import controller.ThreadSapo;
+
 public class Principal {
 
 	public static void main(String[] args) {
+		
+		System.out.println(" == A corrida começou! == ");
+		
+		for (int i = 0; i < 5; i++) {
+			Thread threadSapo = new ThreadSapo();
+			threadSapo.start();
+		}
 
 	}
 
